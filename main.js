755b277cc083bf8900b80f2814dc8f3aa2a6d6ff
Adalberto R. Ribeiro <beto1821@uol.com.br>
2025-11-17 14:45:24 -0300

    fix home

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Função para carregar um componente HTML em um elemento
+  const loadComponent = (elementId, filePath) => {
+    fetch(filePath)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById(elementId).innerHTML = data;
+      })
+      .catch(error => console.error(`Erro ao carregar ${filePath}:`, error));
+  };
+
+  // Carrega o header e o footer
+  loadComponent("header-placeholder", "header.html");
+  loadComponent("footer-placeholder", "footer.html");
+});
