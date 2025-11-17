479bb428daeea4e36e3ffec4fae1f5df799647f9
Adalberto R. Ribeiro <beto1821@uol.com.br>
2025-11-17 19:57:49 -0300

    feat: organiza landing page com SimpleHeader fixo e Header na Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SimpleHeader from './components/SimpleHeader';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
@@ -10,6 +11,7 @@ function App() {
   return (
     <Router>
       <div>
+        <SimpleHeader />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sobre" element={<Sobre />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ import Header from '../components/Header';
 
 const Home = () => {
   return (
-    <>
+    <div className="home-page">
       <Header />
-    </>
+    </div>
   );
 };
 
